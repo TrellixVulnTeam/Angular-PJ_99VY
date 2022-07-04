ad7a536780b513c1ab75daec5b15135983891f08
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2022-07-04 18:08:09 +0900

    2022/07/04
    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { ChartComponent } from './chart/chart.component';
-import { RouterModule, Routes } from "@angular/router";
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
   declarations: [
     AppComponent,
-    ChartComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot()
+    AdminLayoutComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
